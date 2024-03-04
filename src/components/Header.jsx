afcf72e4b91b979939e8417150f82ae2b3938fdf
Gabi Draghici <draghici.gabi89@gmail.com>
2024-03-04 23:52:27 +0000

    Projects Gallery and Projects jsx files updated. Project screenshots added to img folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,27 +7,27 @@ function Header() {
         <header className='header'>
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-            <Link className="navbar-brand" to="/">Gabriela Draghici</Link>
-            <div className="d-flex flex-row justify-content-end">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/projectGallery">Project Gallery</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/projects">Projects</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </div>
-        </div>
-      </nav>
+                <Link className="navbar-brand" to="/">Gabriela Draghici</Link>
+                <div className="d-flex flex-row justify-content-end">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/projectGallery">Project Gallery</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/projects">Projects</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+          </nav>
         </header>
-    )
+    );
 }
 
 export default Header;
--- src/pages/ProjectGallery.jsx
+++ src/pages/ProjectGallery.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import projectsData from '../utils/projectsData'
 
 const ProjectGallery = () => {
   return (
-    <div>ProjectGallery</div>
-  )
+    <div className='container mt-5'>
+      <div className='row'>
+        {projectsData.map((project, index) =>
+        <div key={index}
+        className='col-md-4 mb-4'>
+          <Project project={project} />
+          </div>
+          )}
+      </div>
+    </div>
+  );
 }
 
-export default ProjectGallery
+export default ProjectGallery;

8c95c4ff0cc9772cd14d175925f510d6360516c5
Gabi Draghici <draghici.gabi89@gmail.com>
2024-03-04 22:29:14 +0000

    App component updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { React, useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+const App = () => {
   return (
-    <>
-      Hello world
-    </>
-  )
+    <main>
+     <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/ProjectGallery" element={<ProjectGallery />} />
+          <Route path="/Project" element={<Project />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      
+    </Router>
+    </main>
+  );
 }
 
-export default App
+export default App;
